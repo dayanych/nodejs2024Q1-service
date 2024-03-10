ba23e37bd541ba77e109c9a7f794f794bee7b2b9
dayanych <dayana.shakenova20@gmail.com>
2024-03-10 13:04:05 +0500

    refactor: update imports

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { getAppConfig } from './config/app.config';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { getAppConfig } from './config/app.config';
 
 async function bootstrap() {
   const appConfig = getAppConfig();
